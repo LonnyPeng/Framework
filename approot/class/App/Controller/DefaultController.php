22952d349387d9f66f4467e8e35affe3ec9575c1
lonny <xingchenyekong@163.com>
2017-08-30 10:10:25 +0800

    cahnge

---
+++ approot/class/App/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+class DefaultController extends AbstractActionController
+{
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function indexAction()
+    {
+        return array(
+        	'str' => 'Hello world',
+        );
+    }
+}
